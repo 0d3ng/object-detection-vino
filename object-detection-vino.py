ec7f7dc2413654d87d34921f41366fc742c94ed0
od3ng <lepengdados@gmail.com>
2019-06-23 22:48:15 +0700

    updated: 1. add classes file 2. add name of class in box

--- object-detection-vino.py
+++ object-detection-vino.py
@@ -7,13 +7,27 @@
 #  Website: sinaungoding.com
 
 import cv2 as cv
+import os
+
+# Load class txt
+LABELS = open("classes.txt").read().strip().split("\n")
+
 # Load the model.
 net = cv.dnn.readNet('frozen_inference_graph.xml',
                      'frozen_inference_graph.bin')
 # Specify target device.
 net.setPreferableTarget(cv.dnn.DNN_TARGET_MYRIAD)
+
+font_scale = 1
+font = cv.FONT_HERSHEY_PLAIN
+rectangle_bgr = (255, 255, 255)
+
 # Read an image.
 frame = cv.imread('objects.jpg')
+
+rows = frame.shape[0]
+cols = frame.shape[1]
+
 # Prepare input blob and perform an inference.
 blob = cv.dnn.blobFromImage(frame, size=(672, 384), ddepth=cv.CV_8U)
 net.setInput(blob)
@@ -21,11 +35,26 @@ out = net.forward()
 # Draw detected faces on the frame.
 for detection in out.reshape(-1, 7):
     confidence = float(detection[2])
-    xmin = int(detection[3] * frame.shape[1])
-    ymin = int(detection[4] * frame.shape[0])
-    xmax = int(detection[5] * frame.shape[1])
-    ymax = int(detection[6] * frame.shape[0])
-    if confidence > 0.5:
-        cv.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
+    class_id = int(detection[1])
+    left = detection[3] * cols
+    top = detection[4] * rows
+    right = detection[5] * cols
+    bottom = detection[6] * rows
+    if confidence > 0.8:
+        left = detection[3] * cols
+        top = detection[4] * rows
+        right = detection[5] * cols
+        bottom = detection[6] * rows
+        text = "{} {:.2f}".format(LABELS[class_id], confidence)
+
+        (text_width, text_height) = cv.getTextSize(text, font, fontScale=font_scale, thickness=1)[0]
+        text_offset_x = int(left)
+        text_offset_y = int(top) - 2
+        box_coord = ((text_offset_x, text_offset_y), (text_offset_x + text_width-2, text_offset_y - text_height - 2))
+
+        cv.rectangle(frame, (int(left), int(top)), (int(right), int(bottom)), (255, 255, 255), thickness=2)
+        cv.rectangle(frame, box_coord[0], box_coord[1], rectangle_bgr, cv.FILLED)
+        cv.putText(frame, text, (text_offset_x, text_offset_y), font, fontScale=font_scale, color=(0, 0, 0),thickness=1)
+
 # Save the frame to an image file.
 cv.imwrite('out.png', frame)
